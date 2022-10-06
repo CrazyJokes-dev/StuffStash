9c0099ab4195a890249111cd61de1f427fb4955f
mpochowi <90714974+mpochowi@users.noreply.github.com>
2022-10-06 09:57:54 -0400

    Style: Login FOrm * style: created a login page for the user to login * used bootstrap to create form

--- Stuff-Stash/client/src/App.js
+++ Stuff-Stash/client/src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
-import Navbar from './Pages/navbar';
-import { BrowserRouter as Redirect, Route, Switch } from 'react-router-dom';
+import "./App.css";
+import Navbar from "./Pages/navbar";
+import { BrowserRouter as Redirect, Route, Switch } from "react-router-dom";
 
 import Home from "./Pages/Home";
-import Helpme from './Pages/ListOfUsers';
-import React from 'react';
+import Helpme from "./Pages/ListOfUsers";
+import UserLogin from "./Pages/login";
+import React from "react";
 
 function App() {
   return (
@@ -13,14 +14,14 @@ function App() {
       <div className="content">
         <Navbar />
         <Switch>
-          <Route path="/list" component={Helpme} /> 
+          <Route path="/login" component={UserLogin} />
+          <Route path="/list" component={Helpme} />
           <Route path="/" component={Home} />
-          <Redirect from='*' to='/' />
+          <Redirect from="*" to="/" />
         </Switch>
       </div>
     </div>
     //</Router>
   );
-} 
+}
 export default App;
-
