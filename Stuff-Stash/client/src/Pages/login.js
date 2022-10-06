09392fc7823692ac63d4f349185abfcf5f4a5b23
mpochowi <90714974+mpochowi@users.noreply.github.com>
2022-10-06 10:13:40 -0400

    Style: Added more styling to login page * centered text and boxes for user login * fixed the word remember to be spelled correctly * added link to navbar

--- Stuff-Stash/client/src/Pages/login.js
+++ Stuff-Stash/client/src/Pages/login.js
@@ -2,23 +2,31 @@ import { Button, Form } from "react-bootstrap";
 
 function UserLogin() {
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formUsername">
-        <Form.Label>Username</Form.Label>
-        <Form.Control type="username" placeholder="Enter username" />
-      </Form.Group>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm"></div>
+        <div class="col-sm">
+          <Form>
+            <Form.Group className="mb-3" controlId="formUsername">
+              <Form.Label>Username</Form.Label>
+              <Form.Control type="username" placeholder="Enter username" />
+            </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formKeepLoggedIn">
-        <Form.Check type="checkbox" label="Remeber Me" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Login
-      </Button>
-    </Form>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formKeepLoggedIn">
+              <Form.Check type="checkbox" label="Remember Me" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Login
+            </Button>
+          </Form>
+        </div>
+        <div class="col-sm"></div>
+      </div>
+    </div>
   );
 }
 
