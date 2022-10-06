a2dea1ab21cac54a065ab2c453e431d99a1cf155
Hussein Alsowadi <halsowadi1@gmail.com>
2022-10-06 10:09:28 -0400

    Feat: Started backend for organization registration
    * Feat: Started backend for organization registration
    * Created organization model and collection
    * Updated index.js to include api calls for organization
    
    Please enter the commit message for your changes. Lines starting

--- Stuff-Stash/client/src/Pages/Home.js
+++ Stuff-Stash/client/src/Pages/Home.js
@@ -4,94 +4,99 @@
 //frontend link
 //https://techstack-demo-deployment.ue.r.appspot.com
 
-import '../App.css';
+import "../App.css";
 import { useState, useEffect } from "react";
 import Axios from "axios";
 
-const Home = () =>{
-
-  
-
+const Home = () => {
   const [listOfUsers, setListOfUsers] = useState([]);
   const [recentUser, setRecentUser] = useState([]);
   const [name, setName] = useState("");
 
   const fetchUser = async () => {
-    const res = await fetch('https://api-dot-techstack-demo-deployment.ue.r.appspot.com/api/v1/users');
+    // const res = await fetch('https://api-dot-techstack-demo-deployment.ue.r.appspot.com/api/v1/users');
+    const res = await fetch("https:localhost3000/api/v1/users");
     const data = await res.json();
     //console.log("users/ - DATA ", data);
     //setRecentUser(data.data);
     setRecentUser(data);
   };
-    
-  
+
   const fetchUsers = async () => {
-    const res = await fetch('https://api-dot-techstack-demo-deployment.ue.r.appspot.com/api/v1/users/getUsers');
+    // const res = await fetch('https://api-dot-techstack-demo-deployment.ue.r.appspot.com/api/v1/users/getUsers');
+    const res = await fetch("https:localhost3000/api/v1/users");
     const data = await res.json();
     //console.log("users/getUsers - DATA:", data);
     //setListOfUsers(data.data);
     setListOfUsers(data);
   };
-    
+
   const createUser = async (e) => {
     e.preventDefault();
-    const res = await fetch('https://api-dot-techstack-demo-deployment.ue.r.appspot.com/api/v1/users', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        name
-      })
-    })
-    
+    const res = await fetch(
+      "https://api-dot-techstack-demo-deployment.ue.r.appspot.com/api/v1/users",
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name,
+        }),
+      }
+    );
+
     const data = res.json();
-    console.log('data -- ', data);
+    console.log("data -- ", data);
     if (data.success) {
-      await fetchUsers()
+      await fetchUsers();
     }
-    setName('');
+    setName("");
     //Axios.post("/createUser", {
     //  name: name,
     //}).then((response) => {
-    //  setListOfUsers([...listOfUsers, 
+    //  setListOfUsers([...listOfUsers,
     //    {
-    //      name: name, 
+    //      name: name,
     //    },
     //  ]);
     //});
   };
 
   useEffect(() => {
-    fetchUser()
-  })
+    fetchUser();
+  });
 
   useEffect(() => {
-    fetchUsers()
-  }, [])
+    fetchUsers();
+  }, []);
 
-return (
-
+  return (
     <div className="App">
       <div className="usersDisplay">
-        
         {recentUser.map((user) => {
           return (
             <div>
               <h1>Hello {user.name}</h1>
             </div>
-          );  
+          );
         })}
       </div>
 
       <div>
-        <input id='userName' type="text" placeholder="Name..." onChange={(event) => {setName(event.target.value);}} />
+        <input
+          id="userName"
+          type="text"
+          placeholder="Name..."
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
         <button onClick={createUser}> Greet User </button>
       </div>
     </div>
-  
   );
-}
+};
 
 export default Home;
--- Stuff-Stash/server/index.js
+++ Stuff-Stash/server/index.js
@@ -1,95 +1,93 @@
 const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const UserModel = require('./models/user');
+const mongoose = require("mongoose");
+const UserModel = require("./models/user");
 
-const OrgModel = require('./models/OrgModel');
-const cors = require('cors');
-const PORT = process.env.PORT || 3000
-const users = require('./routes/users');
-const orgs = require('./routes/orgs');
+const OrgModel = require("./models/OrgModel");
+const cors = require("cors");
+const PORT = process.env.PORT || 3000;
+const users = require("./routes/users");
+const orgs = require("./routes/orgs");
 
 app.use(express.json());
 app.use(cors());
 
 mongoose.connect(
-    "mongodb+srv://estefan:teamwork@cluster0.qf1w4nh.mongodb.net/TechStartUp?retryWrites=true&w=majority"
+  "mongodb+srv://estefan:teamwork@cluster0.qf1w4nh.mongodb.net/TechStartUp?retryWrites=true&w=majority"
 );
 
 app.get("/api/v1/users/", (req, res) => {
-    UserModel.find({}, (err, result) => {
-        if (err) {
-            res.json(err);
-        } else {
-            res.json(result);
-        }}).limit(1).sort({$natural:-1});
+  UserModel.find({}, (err, result) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(result);
+    }
+  })
+    .limit(1)
+    .sort({ $natural: -1 });
 });
 
 app.get("/api/v1/users/getUsers", (req, res) => {
-    UserModel.find({}, (err, result) => {
-        if (err) {
-            res.json(err);
-        } else {
-            res.json(result);
-        }
-    });
+  UserModel.find({}, (err, result) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(result);
+    }
+  });
 });
 
 app.post("/api/v1/users/createUser", async (req, res) => {
-     const user = req.body;
-     const newUser = new UserModel(user);
-     await newUser.save();
+  const user = req.body;
+  const newUser = new UserModel(user);
+  await newUser.save();
 
-     res.json(user);
+  res.json(user);
 });
 
-app.get('/', (req, res) => {
-    res.send({msg:'hello world'})
-})
+app.get("/", (req, res) => {
+  res.send({ msg: "hello world" });
+});
 
-app.use('/api/v1/users', users)
-
-
-
-
-
-
-
-
-
+app.use("/api/v1/users", users);
 
 //ORGINZATION API REQUESTS
+
+//This finds most recent Org
 app.get("/api/v1/orgs", (req, res) => {
-    OrgModel.find({}, (err, result) => {
-        if (err) {
-            res.json(err);
-        } else {
-            res.json(result);
-        }}).limit(1).sort({$natural:-1});
+  OrgModel.find({}, (err, result) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(result);
+    }
+  })
+    .limit(1)
+    .sort({ $natural: -1 });
 });
 
+//All Orgs
 app.get("/api/v1/orgs/getOrgs", (req, res) => {
-    OrgModel.find({}, (err, result) => {
-        if (err) {
-            res.json(err);
-        } else {
-            res.json(result);
-        }
-    });
+  OrgModel.find({}, (err, result) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(result);
+    }
+  });
 });
 
 app.post("/api/v1/users/createOrg", async (req, res) => {
-     const org = req.body;
-     const newOrg = new OrgModel(org);
-     await newOrg.save();
+  const org = req.body;
+  const newOrg = new OrgModel(org);
+  await newOrg.save();
 
-     res.json(org);
+  res.json(org);
 });
 
-
-
-app.use('/api/v1/orgs', orgs)
+app.use("/api/v1/orgs", orgs);
 
 app.listen(PORT, () => {
-    console.log("SERVER LISTENING ON PORT ", PORT);
+  console.log("SERVER LISTENING ON PORT ", PORT);
 });
