dae889190c8fc7834ea5c56f417e22eb40c06119
mpochowi <90714974+mpochowi@users.noreply.github.com>
2022-10-06 19:01:32 -0400

    Style: Added more styling to login page * added a container to hold the information * centered the content of the overall form * added a border around the form and user image

--- Stuff-Stash/client/src/Pages/login.js
+++ Stuff-Stash/client/src/Pages/login.js
@@ -3,64 +3,66 @@ import React from "react";
 function UserLogin() {
   return (
     <React.Fragment>
-      <img
-        src="https://img.icons8.com/bubbles/100/000000/user.png"
-        class="img-thumbnail"
-        alt="user"
-      />
+      <span class="border border-1">
+        <div class="container-sm bg-secondary text-light">
+          <img
+            src="https://img.icons8.com/bubbles/100/000000/user.png"
+            class="img-thumbnail"
+            alt="user"
+          />
 
-      <div class="container">
-        <div class="row">
-          <div class="col-sm"></div>
-          <div class="col-sm">
-            <h2>User Login</h2>
-            <form action="/action_page.php">
-              <div class="form-group">
-                <label for="Username" class="text-primary">
-                  Username:
-                </label>
-                <input
-                  type="Username"
-                  class="form-control"
-                  id="Username"
-                  placeholder="Enter username"
-                  name="Username"
-                />
-              </div>
+          <div class="row">
+            <div class="col-sm"></div>
+            <div class="col-sm">
+              <h2>User Login</h2>
+              <form action="/action_page.php" class="justify-content-center">
+                <div class="form-group">
+                  <label for="Username" class="text-white">
+                    Username:
+                  </label>
+                  <input
+                    type="Username"
+                    class="form-control"
+                    id="Username"
+                    placeholder="Enter username"
+                    name="Username"
+                  />
+                </div>
 
-              <div class="form-group">
-                <label for="pwd" class="text-primary">
-                  Password:
-                </label>
-                <input
-                  type="password"
-                  class="form-control"
-                  id="pwd"
-                  placeholder="Enter password"
-                  name="pwd"
-                />
-              </div>
+                <div class="form-group">
+                  <label for="pwd" class="text-white">
+                    Password:
+                  </label>
+                  <input
+                    type="password"
+                    class="form-control"
+                    id="pwd"
+                    placeholder="Enter password"
+                    name="pwd"
+                  />
+                </div>
 
-              <div class="form-check">
-                <input
-                  class="form-check-label col-sm-1"
-                  type="checkbox"
-                  value=""
-                  id="defaultCheck1"
-                />
-                <label class="form-check-label" for="stay logged in">
-                  Remember Me
-                </label>
-              </div>
+                <div class="form-check">
+                  <input
+                    class="form-check-label col-sm-1"
+                    type="checkbox"
+                    value=""
+                    id="defaultCheck1"
+                  />
+                  <label class="form-check-label" for="stay logged in">
+                    Remember Me
+                  </label>
+                </div>
 
-              <button type="login" class="btn btn-primary">
-                Login
-              </button>
-            </form>
+                <button type="login" class="btn btn-primary">
+                  Login
+                </button>
+              </form>
+            </div>
+            <div class="col-sm"></div>
           </div>
-          <div class="col-sm"></div>
         </div>
-      </div>
+      </span>
     </React.Fragment>
   );
 }
