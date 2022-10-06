7753cab5569d26c85e450e4183f8d6de8e8b2678
mpochowi <90714974+mpochowi@users.noreply.github.com>
2022-10-06 17:02:16 -0400

    Style: Fixed spacing and added image * fixed spacing issue between the checkbox and words "remember me". Inserted a space between the two. * added a link to an image icon that shows a user on the login page

--- Stuff-Stash/client/src/Pages/login.js
+++ Stuff-Stash/client/src/Pages/login.js
@@ -2,58 +2,66 @@ import React from "react";
 
 function UserLogin() {
   return (
-    <div class="container">
-      <div class="row">
-        <div class="col-sm"></div>
-        <div class="col-sm">
-          <h2>User Login</h2>
-          <form action="/action_page.php">
-            <div class="form-group">
-              <label for="Username" class="text-primary">
-                Username:
-              </label>
-              <input
-                type="Username"
-                class="form-control"
-                id="Username"
-                placeholder="Enter username"
-                name="Username"
-              />
-            </div>
+    <React.Fragment>
+      <img
+        src="https://img.icons8.com/bubbles/100/000000/user.png"
+        class="img-thumbnail"
+        alt="user"
+      />
 
-            <div class="form-group">
-              <label for="pwd" class="text-primary">
-                Password:
-              </label>
-              <input
-                type="password"
-                class="form-control"
-                id="pwd"
-                placeholder="Enter password"
-                name="pwd"
-              />
-            </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-sm"></div>
+          <div class="col-sm">
+            <h2>User Login</h2>
+            <form action="/action_page.php">
+              <div class="form-group">
+                <label for="Username" class="text-primary">
+                  Username:
+                </label>
+                <input
+                  type="Username"
+                  class="form-control"
+                  id="Username"
+                  placeholder="Enter username"
+                  name="Username"
+                />
+              </div>
 
-            <div class="form-check">
-              <input
-                class="form-check-input"
-                type="checkbox"
-                value=""
-                id="defaultCheck1"
-              />
-              <label class="form-check-label" for="stay logged in">
-                Remember Me
-              </label>
-            </div>
+              <div class="form-group">
+                <label for="pwd" class="text-primary">
+                  Password:
+                </label>
+                <input
+                  type="password"
+                  class="form-control"
+                  id="pwd"
+                  placeholder="Enter password"
+                  name="pwd"
+                />
+              </div>
 
-            <button type="login" class="btn btn-primary">
-              Login
-            </button>
-          </form>
+              <div class="form-check">
+                <input
+                  class="form-check-label col-sm-1"
+                  type="checkbox"
+                  value=""
+                  id="defaultCheck1"
+                />
+                <label class="form-check-label" for="stay logged in">
+                  Remember Me
+                </label>
+              </div>
+
+              <button type="login" class="btn btn-primary">
+                Login
+              </button>
+            </form>
+          </div>
+          <div class="col-sm"></div>
         </div>
-        <div class="col-sm"></div>
       </div>
-    </div>
+    </React.Fragment>
   );
 }
 
