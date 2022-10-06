d724e4a83adfa104edefd275ace05921cd263de6
unknown <lyousef@emich.edu>
2022-10-06 10:46:20 -0400

    style: changed user registration form style *added spacing *changed color of the submit button

--- Stuff-Stash/client/src/components/registrationform.jsx
+++ Stuff-Stash/client/src/components/registrationform.jsx
@@ -1,5 +1,4 @@
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
+import React from "react";
 
 function RegForm() {
   return (
@@ -7,37 +6,56 @@ function RegForm() {
       <div class="row">
         <div class="col-sm"></div>
         <div class="col-sm">
-          <Form>
-            <fieldset disabled>
-              <Form.Group className="mb-3">
-                <Form.Label htmlFor="Username">Username</Form.Label>
-                <Form.Control id="Username" placeholder="Enter username" />
-              </Form.Group>
-              <Form.Group className="mb-3">
-                <Form.Label htmlFor="Password">New Password</Form.Label>
-                <Form.Control id="Password" placeholder="Enter new password" />
-              </Form.Group>
-              <Form.Group className="mb-3">
-                <Form.Label htmlFor="Repeat Password">
-                  Retype Password
-                </Form.Label>
-                <Form.Control
-                  id="Repeat Password"
-                  placeholder="Enter password again"
-                />
-              </Form.Group>
-              <Form.Group className="mb-3">
-                <Form.Label htmlFor="Organization ID">
-                  Organization ID (Optional)
-                </Form.Label>
-                <Form.Control
-                  id="Organization ID"
-                  placeholder="Enter Organization ID"
-                />
-              </Form.Group>
-              <Button type="submit">Submit</Button>
-            </fieldset>
-          </Form>
+          <h2>Create an Account</h2>
+          <form>
+            <div class="form-group m-3">
+              <label for="Username">Username:</label>
+              <input
+                type="Username"
+                class="form-control"
+                id="Username"
+                placeholder="Enter username"
+                name="Username"
+              />
+            </div>
+
+            <div class="form-group m-3">
+              <label for="pwd">Password:</label>
+              <input
+                type="password"
+                class="form-control"
+                id="pwd"
+                placeholder="Enter password"
+                name="pwd"
+              />
+            </div>
+
+            <div class="form-group m-3">
+              <label class="form-label" for="repeat password">
+                Repeat Password
+              </label>
+              <input
+                type="repeat password"
+                id="repeat password"
+                class="form-control"
+                placeholder="Enter password again"
+              />
+            </div>
+            <div class="form-group m-3">
+              <label class="form-label" for="organization ID">
+                Organization ID (Optional)
+              </label>
+              <input
+                type="Organization ID"
+                id="Organization ID"
+                class="form-control"
+                placeholder="Enter organization ID"
+              />
+            </div>
+            <button type="submit" class="btn btn-primary m-3">
+              Submit
+            </button>
+          </form>
         </div>
         <div class="col-sm"></div>
       </div>
