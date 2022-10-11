2a0e921c064092dde8a9725ee5c91d155910bfc2
Estefan Valencia <evalenci@emich.edu>
2022-10-11 10:37:06 -0400

    fix: connected the frontend of user signup to the backend
    * fix: connected the frontend of user signup to the backend

--- Stuff-Stash/client/src/components/registrationform.jsx
+++ Stuff-Stash/client/src/components/registrationform.jsx
@@ -1,13 +1,48 @@
 import React from "react";
+import { useState } from "react";
+import { useHistory } from "react-router-dom"
 
 function RegForm() {
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [organizationID, setOrganizationID] = useState("");
+
+  let history = useHistory();
+
+  const createUser = async (e) => {
+    e.preventDefault();
+    // const res = await fetch('https://api-dot-techstack-demo-deployment.ue.r.appspot.com/api/v1/users', {
+    const res = await fetch('http://localhost:3000/api/v1/users/createUser', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username,
+        password,
+        organizationID
+      })
+    })
+    
+    const data = res.json();
+    console.log('data -- ', data);
+    console.log(res.status);
+    if (res.status !== 400 ) {
+      history.push("/");
+    }
+    setUsername('');
+    setPassword('');
+    setOrganizationID('');
+  };
+
   return (
     <div class="container">
       <div class="row">
         <div class="col-sm"></div>
         <div class="col-sm">
           <h2>Create an Account</h2>
-          <form>
+          <form onSubmit={createUser}>
             <div class="form-group m-3">
               <label for="Username">Username:</label>
               <input
@@ -16,6 +51,7 @@ function RegForm() {
                 id="Username"
                 placeholder="Enter username"
                 name="Username"
+                onChange={(event) => {setUsername(event.target.value);}}
               />
             </div>
 
@@ -27,6 +63,7 @@ function RegForm() {
                 id="pwd"
                 placeholder="Enter password"
                 name="pwd"
+                onChange={(event) => {setPassword(event.target.value);}}
               />
             </div>
 
@@ -50,6 +87,7 @@ function RegForm() {
                 id="Organization ID"
                 class="form-control"
                 placeholder="Enter organization ID"
+                onChange={(event) => {setOrganizationID(event.target.value);}}
               />
             </div>
             <button type="submit" class="btn btn-primary m-3">
