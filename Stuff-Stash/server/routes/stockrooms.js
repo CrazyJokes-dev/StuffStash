1715a4c75bcec6a2f4ccb0f9c5591e049378978d
Sean Mick <smick@emich.edu>
2022-10-11 10:46:24 -0400

    *FEAT: completed backend framework todo: fix cannot POST

--- Stuff-Stash/server/routes/stockrooms.js
+++ Stuff-Stash/server/routes/stockrooms.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const router = express.Router;
-const {createStockroom} = require('..controllers/stockrooms.js');
+const router = express.Router();
+const {createStockroom} = require('../controllers/stockrooms.js');
 
 router
-    .route('/stockrooms')
+    .route('/')
     .post(createStockroom)
 
 module.exports = router;
