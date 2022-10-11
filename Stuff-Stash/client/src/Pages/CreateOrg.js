4a35a7192f44910199505aab078828ac8c525bef
Hussein Alsowadi <halsowadi1@gmail.com>
2022-10-11 11:13:49 -0400

    Feature: Encrypt OrgAccessCode -Encrypted OrgAccessCode with bcrypt -When a new ORg and access code is added, the access code will now be encrypted in the database

--- Stuff-Stash/client/src/Pages/CreateOrg.js
+++ Stuff-Stash/client/src/Pages/CreateOrg.js
@@ -4,88 +4,88 @@
 //frontend link
 //https://techstack-demo-deployment.ue.r.appspot.com
 
-import '../App.css';
+import "../App.css";
 import { useState, useEffect } from "react";
 import Axios from "axios";
 
-const CreateOrg = () =>{
+const CreateOrg = () => {
+  const [name, setorgname] = useState(" ");
+  const [OrgAccessCode, setorgCode] = useState(" ");
+  const [ListOfOrgs, setListOfOrgs] = useState([]);
 
-    const [name,setorgname]=useState(" ")
-    const [OrgAccessCode,setorgCode]=useState(" ")
-    const [ListOfOrgs, setListOfOrgs] = useState([]);
-  
-
- // const fetchOrg = async () => {
-   // const res = await fetch('https://api-dot-techstack-demo-deployment.ue.r.appspot.com/api/v1/orgs');
-    //const res = await fetch('http://localhost:3000/api/v1/users');
-   // const data = await res.json();
-    //console.log("users/ - DATA ", data);  
-    //setRecentUser(data.data);
-    //setRecentUser(data);
+  // const fetchOrg = async () => {
+  // const res = await fetch('https://api-dot-techstack-demo-deployment.ue.r.appspot.com/api/v1/orgs');
+  //const res = await fetch('http://localhost:3000/api/v1/users');
+  // const data = await res.json();
+  //console.log("users/ - DATA ", data);
+  //setRecentUser(data.data);
+  //setRecentUser(data);
   //};
-    
-  
+
   //get your orgs
   const fetchOrgs = async () => {
-    const res = await fetch('https://api-dot-techstack-demo-deployment.ue.r.appspot.com/api/v1/orgs/getOrgs');
+    const res = await fetch(
+      "https://api-dot-techstack-demo-deployment.ue.r.appspot.com/api/v1/orgs/getOrgs"
+    );
     const data = await res.json();
-   console.log("users/getUsers - DATA:", data);
-   setListOfOrgs(data); 
+    console.log("users/getUsers - DATA:", data);
+    setListOfOrgs(data);
   };
-    
 
   // Adds new Org
   const addOrg = async (e) => {
     e.preventDefault();
     //const res = await fetch('https://api-dot-techstack-demo-deployment.ue.r.appspot.com/api/v1/users/createOrg', {
-    const res = await fetch('http://localhost:3000/api/v1/users/createOrg', {
-      method: 'POST',
+    const res = await fetch("http://localhost:3000/api/v1/users/createOrg", {
+      method: "POST",
       headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
+        Accept: "application/json",
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         name,
-        OrgAccessCode
-      })
-    })
-    
+        OrgAccessCode,
+      }),
+    });
+
     const data = res.json();
-    console.log('data -- ', data);
+    console.log("data -- ", data);
     if (data.success) {
-      await fetchOrgs()
+      await fetchOrgs();
     }
-
- };
-
- useEffect(() => {
-    fetchOrgs()
-  })
+  };
 
   useEffect(() => {
-    fetchOrgs()
-  }, [])
+    fetchOrgs();
+  });
 
-return (
+  useEffect(() => {
+    fetchOrgs();
+  }, []);
 
-   <div className="App">
-     <div className="usersDisplay">
-        
-     
-     </div>
+  return (
+    <div className="App">
+      <div className="usersDisplay"></div>
 
       <div>
-      <form>
-            <h1>Enter an organization name:</h1>
-            <input type="text" value={name} onChange={(e)=>setorgname(e.target.value)}></input>
-            <h1>Enter an organization id:</h1>
-            <input type="text" value={OrgAccessCode} onChange={(e)=>setorgCode(e.target.value)}></input>
-            <button onClick={addOrg}>Submit</button>
-            </form>
+        <form>
+          <h1>Enter an organization name:</h1>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setorgname(e.target.value)}
+          />
+          <h1>Enter an organization id:</h1>
+          <input
+            type="text"
+            value={OrgAccessCode}
+            onChange={(e) => setorgCode(e.target.value)}
+          />
+          <button onClick={addOrg}>Submit</button>
+        </form>
       </div>
     </div>
-  
   );
-}
+};
 
 export default CreateOrg;
--- Stuff-Stash/server/index.js
+++ Stuff-Stash/server/index.js
@@ -8,7 +8,8 @@ const cors = require("cors");
 const PORT = process.env.PORT || 3000;
 const users = require("./routes/users");
 const orgs = require("./routes/orgs");
-
+const bcrypt = require("bcrypt");
+  const saltRounds = 12; // <-- The lower the number the more hashes per second. Higher = less hashes per second
 app.use(express.json());
 app.use(cors());
 
@@ -78,15 +79,58 @@ app.get("/api/v1/orgs/getOrgs", (req, res) => {
   });
 });
 
-app.post("/api/v1/users/createOrg", async (req, res) => {
-  const org = req.body;
-  const newOrg = new OrgModel(org);
-  await newOrg.save();
+// app.post("/api/v1/users/createOrg", async (req, res) => {
+//   const { name, OrgAccessCode } = req.body;
+//   OrgModel.findOne({ OrgAccessCode: OrgAccessCode }).then((org) => {
+//     if (org) return res.status(400).json({ msg: "Org code already exists" });
+//     const newOrg = new OrgModel({
+//       name,
+//       OrgAccessCode,
+//     });
+//     newOrg.save();
+//   });
+// });
 
-  res.json(org);
+app.post("/api/v1/users/createOrg", (req, res) => {
+  const { name, OrgAccessCode } = req.body;
+
+  // Checks to see if the username/password that was entered, wasn't empty.
+  // If it was empty, displays a message on screen telling the user to enter them.
+  //if (OrgAccessCode) {
+  //  return res
+  //    .status(400)
+   //   .json({ msg: "Please enter a username and a password" });
+//  }
+
+  // Checks to see if another username already exists in the database and rejects it if there is one.
+  OrgModel.findOne({ OrgAccessCode: OrgAccessCode }).then((org) => {
+    if (org) return res.status(400).json({ msg: "User already exists" });
+
+    // This creates a model entry into the database with all the current new registration information.
+    const newOrg = new OrgModel({
+      name,
+      OrgAccessCode,
+    });
+
+    // encrypts the password with hashing
+    bcrypt.genSalt(saltRounds, (err, salt) =>
+      bcrypt.hash(newOrg.OrgAccessCode, salt, (err, hash) => {
+        if (err) throw err;
+
+        newOrg.OrgAccessCode = hash;
+
+        // saves the user to the database
+        // must be inside bcrypt.hash() or else the password saved won't be encrypted
+        newOrg
+          .save()
+          .then(res.json({ msg: "Successfully Registered" }))
+          .catch((err) => console.log(err));
+      })
+    );
+  });
 });
 
-app.use("/api/v1/orgs", orgs);
+//app.use("/api/v1/orgs/", orgs);
 
 app.listen(PORT, () => {
   console.log("SERVER LISTENING ON PORT ", PORT);
