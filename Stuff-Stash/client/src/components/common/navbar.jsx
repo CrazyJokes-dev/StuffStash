b53f33b1c8e730c7dc42c2f9bb2b3a994c5fbf27
mpochowi <90714974+mpochowi@users.noreply.github.com>
2022-10-13 10:05:28 -0400

    Stlye: Styled the navbar to look presentable * added a line to the navbar file that separates the company name "Stuff Stash" from the links * looks more presentable than before

--- Stuff-Stash/client/src/components/common/navbar.jsx
+++ Stuff-Stash/client/src/components/common/navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <React.Fragment>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-end">
         <span className="navbar-brand mx-2">Stuff Stash</span>
         <button
           className="navbar-toggler"
@@ -18,8 +18,11 @@ const Navbar = () => {
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
+        <div
+          className="collapse navbar-collapse justify-content-end"
+          id="navbarNav"
+        >
+          <ul className="navbar-nav ">
             <li className="nav-item active">
               <Link to="/" className="nav-link">
                 Home
