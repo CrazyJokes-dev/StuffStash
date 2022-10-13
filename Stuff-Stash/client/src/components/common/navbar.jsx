bc4951a28b5504686f461902556a1f0829c0d982
mpochowi <90714974+mpochowi@users.noreply.github.com>
2022-10-13 09:39:41 -0400

    Style: Added pages and made code changes to fix welcome page * Added bg.css to add coloring to the background * Formated styling of welcome page to take out two columns/division * Made welcome page more presentable

--- Stuff-Stash/client/src/components/common/navbar.jsx
+++ Stuff-Stash/client/src/components/common/navbar.jsx
@@ -17,6 +17,7 @@ const Navbar = () => {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
+
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item active">
@@ -24,6 +25,7 @@ const Navbar = () => {
                 Home
               </Link>
             </li>
+
             <li className="nav-item active">
               <Link to="/registration" className="nav-link">
                 Register
