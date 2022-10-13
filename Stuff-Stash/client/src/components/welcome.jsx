c01287cb51f8d93f08f9f7721c588e684f3adaf4
MSweebe <msweebe@emich.edu>
2022-10-13 09:18:48 -0400

    Style: Add styling to welcome page * Add animated css background * Format page to put content in center of page * Change text color to white

--- Stuff-Stash/client/src/components/welcome.jsx
+++ Stuff-Stash/client/src/components/welcome.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import "./styles/welcome.css";
+import "./styles/bg.css";
 
 const welcome = () => {
   return (
     <React.Fragment>
-      <div className="container-fluid bg-warning h-100 green">
-        <div className="row bg-danger">
-          <div className="col d-flex align-items-center text-center justify-content-center bg-primary">
+      <div className="bg fill d-flex align-items-center justify-content-center area">
+        <div className="row">
+          <div className="col d-flex align-items-center text-center justify-content-center">
             <div className="jumbotron">
-              <h1 className="display-4">Welcome to Stuff Stash</h1>
+              <h1 className="display-1">Welcome to Stuff Stash</h1>
               <p className="lead">The all in one Asset Manager</p>
             </div>
           </div>
-          <div className="col bg-secondary m-5"></div>
+          <div className="col m-5"></div>
         </div>
       </div>
     </React.Fragment>
