bb0e5985cce0bd11d05071b4dbecd20e47f8e95e
smick00 <smick@emich.edu>
2022-10-15 17:43:38 -0400

    FEAT: continued work on linking front/back feat: removed redundant files

--- Stuff-Stash/client/src/Pages/CreateStockRoom.js
+++ Stuff-Stash/client/src/Pages/CreateStockRoom.js
@@ -3,35 +3,42 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
 
-export default function AddOrgUser() {
-  const [stockRoom, setStockRoom] = useState("");
-  const [userID, setUserID] = useState("");
-  function validateForm() {
-    return userID.length > 0 || stockRoom.lengh > 0;
-  }
-  function handleSubmit(event) {
-    event.preventDefault();
-  }
-  return (
+export default function AddStockroom() {
+    const [stockRoomName, setStockRoomName] = useState("");
+    // const [userID, setUserID] = useState("");
+
+    const addStockroom = async (e) => {
+      e.preventDefault();
+      console.log("got here");
+      const res = await fetch('https://api-dot-techstack-demo-deployment.ue.r.appspot.com/api/v1/addStockroom/', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          stockRoomName,
+          "org":"A123"
+          // SESSION STUFF GOES HERE!
+        })
+      })
+      setStockRoomName('');
+    };
+
+
+    return (
     <div className="addCreateStockRoom container">
       <div className="row">
         <div className="col"></div>
         <div className="col">
-          <Form onSubmit="{handleSubmit}">
+          <Form onSubmit={addStockroom}>
             <Form.Group size="sm" controlId="stockRoom orgID">
-              <Form.Label>User ID</Form.Label>
-              <Form.Control
-                autoFocus
-                type="text"
-                value={userID}
-                onChange={(e) => setUserID(e.target.value)}
-              />
               <Form.Label>Enter Stockroom Name</Form.Label>
               <Form.Control
                 autoFocus
                 type="text"
-                value={stockRoom}
-                onChange={(e) => setStockRoom(e.target.value)}
+                value={stockRoomName}
+                onChange={(e) => setStockRoomName(e.target.value)}
               />
             </Form.Group>
 
@@ -40,7 +47,7 @@ export default function AddOrgUser() {
               block
               size="sm"
               type="submit"
-              disabled="{!validateForm()}"
+              // disabled="{!validateForm()}"
             >
               Create Stockroom
             </Button>
