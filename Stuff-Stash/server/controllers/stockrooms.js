17804ce476be43942be1873427a385d64389ebc5
AHearn <ahearn1@emich.edu>
2022-10-16 19:57:29 -0400

    Fix: changed variables

--- Stuff-Stash/server/controllers/stockrooms.js
+++ Stuff-Stash/server/controllers/stockrooms.js
@@ -3,6 +3,7 @@ const Room = require("../models/stockroom"); //import user from models
 
 //check if orgId exists
 const getRoom = asyncHandler(async (req, res) => {
+	//replace with whatever you want to get room by
 	let name = "tester";
 	await Room.find({ owner: name }).then(function (err, result) {
 		if (err) {
