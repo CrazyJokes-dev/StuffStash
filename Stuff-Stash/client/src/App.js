232711245ca49c99e74a44fb65f0355bbb4d39bd
unknown <lyousef@emich.edu>
2022-10-16 20:20:03 -0400

    Feat: route frontend code to ensure it connects with backend *Installed react sessions *Created a POST and Get method *Get and send information to Database

--- Stuff-Stash/client/src/App.js
+++ Stuff-Stash/client/src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
-import Navbar from './Pages/navbar';
-import { BrowserRouter as Redirect, Route, Switch } from 'react-router-dom';
+import "./App.css";
+import Navbar from "./Pages/navbar";
+import { BrowserRouter as Redirect, Route, Switch } from "react-router-dom";
+import { ReactSession } from "react-client-session";
 
 import Home from "./Pages/Home";
-import Helpme from './Pages/ListOfUsers';
-import React from 'react';
+import viewstockroomFrontend from "./Pages/viewstockroomFrontend";
+import Helpme from "./Pages/ListOfUsers";
+import React from "react";
+
+ReactSession.setStoreType("localStorage");
 
 function App() {
   return (
@@ -13,14 +17,14 @@ function App() {
       <div className="content">
         <Navbar />
         <Switch>
-          <Route path="/list" component={Helpme} /> 
+          <Route path="/list" component={Helpme} />
+          <Route path="/viewstockroom" component={viewstockroomFrontend} />
           <Route path="/" component={Home} />
-          <Redirect from='*' to='/' />
+          <Redirect from="*" to="/" />
         </Switch>
       </div>
     </div>
     //</Router>
   );
-} 
+}
 export default App;
-
---
+++ Stuff-Stash/client/src/Pages/viewstockroomFrontend.js
@@ -0,0 +1,46 @@
+import { Button } from "bootstrap";
+import React from "react";
+import { ReactSession } from "react-client-session";
+
+const viewstockroomFrontend = () => {
+  const orgid = ReactSession.get("orgname");
+  const DisplayStockroom = async (e) => {
+    e.preventDefault();
+    //const res = await fetch('https://api-dot-techstack-demo-deployment.ue.r.appspot.com/api/v1/users', {
+
+    const res = await fetch("http://localhost:3000/api/v1/users/viewstock", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        orgid,
+      }),
+    });
+
+    const data = res.json();
+    console.log("data -- ", data);
+    //if (data.success) {
+    //  await fetchUsers()
+    //}
+    //setName('');
+    //Axios.post("/createUser", {
+    //  name: name,
+    //}).then((response) => {
+    //  setListOfUsers([...listOfUsers,
+    //    {
+    //      name: name,
+    //    },
+    //  ]);
+    //});
+  };
+
+  return (
+    <div>
+      <button onClick={DisplayStockroom}>DisplayStockroom</button>
+    </div>
+  );
+};
+
+export default viewstockroomFrontend;
