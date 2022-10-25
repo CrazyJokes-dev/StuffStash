6ba6baaab577f3156a6e5a4b5c78ef80d4cdbaf5
Sean Mick <smick@emich.edu>
2022-10-25 09:46:05 -0400

    FEAT: added very basic asset model contents

--- Stuff-Stash/server/models/Asset.js
+++ Stuff-Stash/server/models/Asset.js
@@ -1,4 +1,16 @@
 const assetSchema = new mongoose.schema({
+    //assets should have a identifier/name,
+    identifier: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    //a "type" (ie, laptop, projector, monitor, etc)
+    category: {
+        type: String,
+        required: true
+    },
+    inUse: {
 
-
+    }
 })
