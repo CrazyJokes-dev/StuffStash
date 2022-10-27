47aeea6fc9de0de4bd68fedfc0d1b3af0541e1f8
mpochowi <90714974+mpochowi@users.noreply.github.com>
2022-10-27 18:33:42 -0400

    Style: added a user icon/image to the display * inserted an image of a sour patch kid * worked on sizing it but need to work on sizing more

--- Stuff-Stash/client/src/components/UserProfile.jsx
+++ Stuff-Stash/client/src/components/UserProfile.jsx
@@ -3,6 +3,7 @@ import { ReactSession } from "react-client-session";
 import { useHistory } from "react-router-dom";
 import React, { Component } from "react";
 
+// <img src={require("./sour_patch.jpg")/>
 const user = ReactSession.get("username");
 //for debug will display name of user currently logged in
 console.log(user);
@@ -10,9 +11,16 @@ console.log(user);
 class UserProfile extends Component {
   render() {
     return (
-      <div className="userDiv">
-        <p>Welcome {user}</p>
-      </div>
+      <React.Fragment>
+        <div className="userDiv">
+          <p>Welcome {user}</p>
+        </div>
+        <div class="col-sm">
+          <div class="container-sm">
+            <img className="imgDiv" src={require("./sour_patch.jpg")} />
+          </div>
+        </div>
+      </React.Fragment>
     );
   }
 }
