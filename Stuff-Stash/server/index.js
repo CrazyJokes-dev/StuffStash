0a76010fcb4ab03641332f549c2e7e543aa77c4e
smick00 <smick@emich.edu>
2022-11-08 09:28:26 -0500

    FIX: OrgView was somehow deleted

--- Stuff-Stash/server/index.js
+++ Stuff-Stash/server/index.js
@@ -265,6 +265,26 @@ app.post("/api/v1/users/viewstock", (req, res) => {
   res.status(200).json({ msg: "why wont you work" });
 });
 
+
+app.get("/api/v1/orgs/OrgView/:userid", (req, res) => {
+  const userid = req.params.userid;
+  //console.log(userid);
+  UserModel.findOne(
+    { username: userid },
+    { "organizationID.name": 1, _id: 0 }
+  ).then((view) => {
+    if (view) {
+      console.log(view);
+      return res.json(view);
+    } else {
+      return res.status(400).json({
+        msg: "Sorry,We did not find any organization for this Username",
+      });
+    }
+  });
+});
+
+
 app.use("/api/v1/orgs/", orgs);
 
 app.use("/api/v1/users", users);
