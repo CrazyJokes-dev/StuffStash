1cdbacb8fe68c583f0c16896705ec117f36cb18f
MSweebe <msweebe@emich.edu>
2022-11-15 09:27:26 -0500

    Maintenance: Client side code clean up * removed comments and unnessecary files * cleaned up index.js for more modern call

--- Stuff-Stash/client/src/index.js
+++ Stuff-Stash/client/src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+// ReactDOM.render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>,
+//   document.getElementById("root")
+// );
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<BrowserRouter><App /></BrowserRouter>);
