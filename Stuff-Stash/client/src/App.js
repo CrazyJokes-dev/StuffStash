b7cb48a3e5fb63ad0307097daaca13ee1e5c977f
AHearn <ahearn1@emich.edu>
2022-12-01 09:29:04 -0500

    Merge

--- Stuff-Stash/client/src/App.js
+++ Stuff-Stash/client/src/App.js
@@ -1,9 +1,10 @@
 import {
-  BrowserRouter as Redirect,
-  Route,
-  Switch,
-  useHistory,
+	BrowserRouter as Redirect,
+	Route,
+	Switch,
+	useHistory,
 } from "react-router-dom";
+
 import Helpme from "./Pages/ListOfUsers";
 import UserLogin from "./Pages/login";
 import React from "react";
@@ -16,6 +17,7 @@ import CreateStockroom from "./Pages/CreateStockRoom";
 import RenameOrgPage from "./components/RenameOrg";
 import AddUserOrg from "./Pages/AddUserOrg";
 import viewstockroomFrontend from "./Pages/viewstockroomFrontend";
+import viewOrgMembers from "./Pages/viewOrgMembers";
 import AssetForm from "./Pages/addAssetForm";
 // sets storage type for session variables
 ReactSession.setStoreType("localStorage");
@@ -23,51 +25,72 @@ ReactSession.setStoreType("localStorage");
 const user = ReactSession.get("username");
 
 function App() {
-  let pathname = window.location.pathname;
-  let history = useHistory();
+	let pathname = window.location.pathname;
+	let history = useHistory();
 
-  function checkRefresh() {
-    console.log(pathname);
-    history.push("/");
-    window.location.reload();
-  }
+	function checkRefresh() {
+		console.log(pathname);
+		history.push("/");
+		window.location.reload();
+	}
 
-  // when this constant is called, it checks if there is a username stored in the session variables
-  const isLoggedIn = () => {
-    if (user != null) {
-      console.log(user);
-      return true;
-    } else {
-      return false;
-    }
-  };
+	// when this constant is called, it checks if there is a username stored in the session variables
+	const isLoggedIn = () => {
+		if (user != null) {
+			console.log(user);
+			return true;
+		} else {
+			return false;
+		}
+	};
 
-  return (
-    <div className="App">
-      {!isLoggedIn() && pathname !== "/" && pathname !== "/reg"
-        ? (checkRefresh(), (pathname = window.location.pathname))
-        : console.log("Rerouting")}
-      <div className="content">
-        <Switch>
-          <Route path="/list" exact component={Helpme} />
-          <Route
-            path="/viewstockroomFrontend"
-            exact
-            component={viewstockroomFrontend}
-          />
-          <Route path="/reg" exact component={RegForm} />
-          <Route path="/dashboard" exact component={Dashboard} />
-          <Route path="/Org" exact component={CreateOrg} />
-          <Route path="/RenameOrg" exact component={RenameOrgPage} />
-          <Route path="/createStockRoom" exact component={CreateStockroom} />
-          <Route path="/adduserOrg" exact component={AddUserOrg} />
-          <Route path="/addAssetForm" exact component={AssetForm} />
+	return (
+		<div className="App">
+			{!isLoggedIn() && pathname !== "/" && pathname !== "/reg"
+				? (checkRefresh(), (pathname = window.location.pathname))
+				: console.log("Rerouting")}
+			<div className="content">
+				<Switch>
+					<Route path="/list" exact component={Helpme} />
+					<Route
+						path="/viewstockroomFrontend"
+						exact
+						component={viewstockroomFrontend}
+					/>
+					<Route
+						path="/viewOrgMember"
+						exact
+						component={viewOrgMembers}
+					/>
+					<Route path="/reg" exact component={RegForm} />
+					<Route path="/dashboard" exact component={Dashboard} />
+					<Route path="/Org" exact component={CreateOrg} />
+					<Route
+						path="/RenameOrg"
+						exact
+						component={RenameOrgPage}
+					/>
+					<Route
+						path="/createStockRoom"
+						exact
+						component={CreateStockroom}
+					/>
+					<Route
+						path="/adduserOrg"
+						exact
+						component={AddUserOrg}
+					/>
+					<Route
+						path="/addAssetForm"
+						exact
+						component={AssetForm}
+					/>
 
-          <Route path="/" exact component={welcome} />
-          <Redirect from="*" to="/" />
-        </Switch>
-      </div>
-    </div>
-  );
+					<Route path="/" exact component={welcome} />
+					<Redirect from="*" to="/" />
+				</Switch>
+			</div>
+		</div>
+	);
 }
 export default App;
--- Stuff-Stash/client/src/Pages/dashboard.js
+++ Stuff-Stash/client/src/Pages/dashboard.js
@@ -5,25 +5,26 @@ import "./styles/dashboardStyles.css";
 import OrgViewDashboard from "../components/orgView";
 import StockRoomViewDashboard from "../components/StockRoomViewDashboard";
 import UserProfile from "../components/UserProfile";
+import ViewMembers from "../components/ViewMembers";
 
 //styles -----------------------------
 
 const styling = {
-  width: "300px",
+	width: "300px",
 };
 
 const linkStyle = {
-  textDecoration: "none",
-  color: "white",
+	textDecoration: "none",
+	color: "white",
 };
 
 //-------------------------------------
 
 const logoutUser = async (e) => {
-  ReactSession.remove("username");
-  ReactSession.remove("orgID");
-  ReactSession.remove("selectedOrg");
-  alert("You are now logged out!");
+	ReactSession.remove("username");
+	ReactSession.remove("orgID");
+	ReactSession.remove("selectedOrg");
+	alert("You are now logged out!");
 };
 
 const username = ReactSession.get("username");
@@ -32,73 +33,105 @@ const currentOrg = ReactSession.get("selectedOrg");
 console.log(currentOrg);
 
 class Dashboard extends Component {
-  state = {};
-  render() {
-    return (
-      <div className="back container-fluid d-flex m-0 p-0">
-        {/** COL 1 -- LEFT SIDE */}
-        <div className="leftDisplay">
-          <div className="container-fluid userDisplay d-flex justify-content-center">
-            {/** Div for welcoming user. Will house any relelvent user info (username, num of orgs in??, num of items checked out??) */}
-            <UserProfile />
-          </div>
+	state = {};
+	render() {
+		return (
+			<div className="back container-fluid d-flex m-0 p-0">
+				{/** COL 1 -- LEFT SIDE */}
+				<div className="leftDisplay">
+					<div className="container-fluid userDisplay d-flex justify-content-center">
+						{/** Div for welcoming user. Will house any relelvent user info (username, num of orgs in??, num of items checked out??) */}
+						<UserProfile />
+					</div>
 
-          <div className="container-fluid orgDisplay p-1 justify-content-center">
-            {/** Div for displaying the orgs relating to user that is signed in. Should be in some sort of scrollable list. Each item will be clickable*/}
-            <span className="display-5 p-3" style={{ color: "white" }}>
-              Current
-            </span>{" "}
-            <br />
-            <span className="display-6 p-3" style={{ color: "white" }}>
-              Organizations
-            </span>
-            <OrgViewDashboard />
-          </div>
-          <div className="d-flex flex-column buttonDisplay p-2 justify-content-center">
-            {/** Div for displaying the buttons to create an org or join existing or via orgID */}
-            <div className="btn btn-success m-1 button button1">
-              <Link to="/adduserOrg" exact style={linkStyle}>
-                Join Existing Organization
-              </Link>
-            </div>
-            <div className="btn btn-success m-1 button button2">
-              <Link to="/org" exact style={linkStyle}>
-                Create Organization
-              </Link>
-            </div>
-            <div className="btn btn-success m-1 button button1">
-              <Link to="/createStockRoom" exact style={linkStyle}>
-                Create Stockroom
-              </Link>
-            </div>
-            <div className="btn btn-success m-1 button button1 ">
-              <Link to="/addAssetForm" exact style={linkStyle}>
-                Create Asset
-              </Link>
-            </div>
-            <div
-              className="btn btn-success m-1 button button1"
-              onClick={logoutUser}
-            >
-              <Link to="/#" exact style={linkStyle}>
-                Logout
-              </Link>
-            </div>
-          </div>
-        </div>
-        {/** COL 2 -- RIGHT SIDE */}
-        <div className="container-fluid rightDisplay col">
-          <div className="stckRoomDisplay justify-content-center">
-            <span className="display-3">Current Stockrooms</span>
-            {/** Div for displaying stockrooms of org when org button or link is clicked*/}
-            <ul className="stockroom-list list-group list-group-flush">
-              <StockRoomViewDashboard />
-            </ul>
-          </div>
-        </div>
-      </div>
-    );
-  }
+					<div className="container-fluid orgDisplay p-1 justify-content-center">
+						{/** Div for displaying the orgs relating to user that is signed in. Should be in some sort of scrollable list. Each item will be clickable*/}
+						<span
+							className="display-5 p-3"
+							style={{ color: "white" }}
+						>
+							Current
+						</span>{" "}
+						<br />
+						<span
+							className="display-6 p-3"
+							style={{ color: "white" }}
+						>
+							Organizations
+						</span>
+						<OrgViewDashboard />
+					</div>
+					<div className="d-flex flex-column buttonDisplay p-2 justify-content-center">
+						{/** Div for displaying the buttons to create an org or join existing or via orgID */}
+						<div className="btn btn-success m-1 button button1">
+							<Link
+								to="/adduserOrg"
+								exact
+								style={linkStyle}
+							>
+								Join Existing Organization
+							</Link>
+						</div>
+						<div className="btn btn-success m-1 button button2">
+							<Link to="/org" exact style={linkStyle}>
+								Create Organization
+							</Link>
+						</div>
+						<div className="btn btn-success m-1 button button1">
+							<Link
+								to="/createStockRoom"
+								exact
+								style={linkStyle}
+							>
+								Create Stockroom
+							</Link>
+						</div>
+						<div className="btn btn-success m-1 button button1 ">
+							<Link
+								to="/addAssetForm"
+								exact
+								style={linkStyle}
+							>
+								Create Asset
+							</Link>
+						</div>
+						<div
+							className="btn btn-success m-1 button button1"
+							onClick={logoutUser}
+						>
+							<Link to="/#" exact style={linkStyle}>
+								Logout
+							</Link>
+						</div>
+					</div>
+				</div>
+				{/** COL 2 -- RIGHT SIDE */}
+				<div className="container-fluid rightDisplay col">
+					<div className="stckRoomDisplay justify-content-center">
+						<span className="display-3">
+							Current Stockrooms
+						</span>
+						{/** Div for displaying stockrooms of org when org button or link is clicked*/}
+						<ul className="stockroom-list list-group list-group-flush">
+							<StockRoomViewDashboard />
+						</ul>
+					</div>
+				</div>
+				{/** COL 3 -- RIGHT SIDE */}
+				<div className="container-fluid rightDisplay col">
+					<div className="stckRoomDisplay justify-content-center">
+						<span className="display-3">
+							Organization Members
+						</span>
+						{/** Div for displaying stockrooms of org when org button or link is clicked*/}
+						<ul className="stockroom-list list-group list-group-flush">
+							<ViewMembers />
+						</ul>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Dashboard;
