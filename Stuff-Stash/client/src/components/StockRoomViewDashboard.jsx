4748bdcf690f8596fee9bad618ec028bcf32d3f1
unknown <lyousef@emich.edu>
2022-12-01 09:47:02 -0500

    FEAT: Created a dropdown from existing button to allow users to create asset within a stockroom view *Feat: added a button *Feat: setsessions function created to link to backend *Feat: provided a way for dropdown button to disappear and reappear on click *Style: Named button "create asset"

--- Stuff-Stash/client/src/components/StockRoomViewDashboard.jsx
+++ Stuff-Stash/client/src/components/StockRoomViewDashboard.jsx
@@ -6,11 +6,12 @@ import Axios from "axios";
 
 const StockRoomViewDashboard = () => {
   const [listOfStockRoom, setListOfStockRoom] = useState([]);
+  const [org, setOrg] = useState("");
+  const [stockroomName, setStockroomName] = useState("");
   //const [orgName, setOrgName] = useState({});
   const [error, setError] = useState();
   let history = useHistory();
   const userid = ReactSession.get("username");
-
   const orgName = ReactSession.get("selectedOrg");
 
   const linkStyle = {
@@ -19,17 +20,49 @@ const StockRoomViewDashboard = () => {
   };
 
   useEffect(() => {
-    Axios.get(
-      `http://localhost:3000/api/v1/users/viewstock/${orgName}`
-    )
+    Axios.get(`http://localhost:3000/api/v1/users/viewstock/${orgName}`)
       .then((response) => {
         setListOfStockRoom(response.data);
       })
       .catch((err) => {
-        setListOfStockRoom("");
         setError(err);
       });
   }, [orgName]);
+
+  var check = false;
+  const handleClick = async (event) => {
+    try {
+      const response = await fetch(
+        "http://localhost:3000/api/v1/deleteStockroom",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            org,
+            stockroomName,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(`Error! status: ${response.status}`);
+      }
+
+      const result = await response.json();
+
+      console.log("result is: ", JSON.stringify(result, null, 4));
+    } catch (err) {
+    } finally {
+      window.location.reload();
+    }
+  };
+
+  const setsessions = (event) => {
+    document.getElementById("CreateAssetButton").hidden = false;
+  };
 
   return (
     <React.Fragment>
@@ -38,12 +71,27 @@ const StockRoomViewDashboard = () => {
           <li className="list-group-item bg-transparent" key={value.name}>
             {Object.entries(value).map((name, key) => {
               return (
-                <div
-                  className="container-fluid buttonItem shadowbtn"
-                  key={name[1]}
-                >
-                  <button className="toggle-btn" data-active="inactive">
-                    <span className="btnLabel">{name[1]}</span>
+                <div>
+                  <div
+                    className="container-fluid buttonItem shadowbtn"
+                    key={name[1]}
+                  >
+                    <button
+                      className="toggle-btn"
+                      data-active="inactive"
+                      id={name[1]}
+                      onClick={setsessions}
+                    >
+                      <span className="btnLabel">{name[1]}</span>
+                    </button>
+                  </div>
+                  <button
+                    id="CreateAssetButton"
+                    onClick={handleClick}
+                    type="hidden"
+                    hidden="true"
+                  >
+                    Create Asset
                   </button>
                 </div>
               );
