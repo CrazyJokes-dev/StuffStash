fe9000cd09b1a399237b9a277b630f5924883b7f
halsowadi <halsowadi1@gmail.com>
2022-12-02 14:03:02 -0500

    Feature:Delete asset *Feature Delete asset *Moved some api code around

--- Stuff-Stash/server/index.js
+++ Stuff-Stash/server/index.js
@@ -391,7 +391,8 @@ app.post("/api/v1/UpdateAsset", async (req, res) => {
       return res.status(200).json({msg:"It worked"});
      });
 
-  app.post("/api/v1/deleteAsset", async (req, res) => {
+});
+app.post("/api/v1/deleteAsset", async (req, res) => {
   console.log("delet asset");
   const stockroom = req.body.stockroomName;
   const { identifier } = req.body;
@@ -404,8 +405,6 @@ app.post("/api/v1/UpdateAsset", async (req, res) => {
       return res.status(200).json({msg:"It worked"});
      });
      
-});
-
 });
 app.use("/api/v1/orgs/", orgs);
 
