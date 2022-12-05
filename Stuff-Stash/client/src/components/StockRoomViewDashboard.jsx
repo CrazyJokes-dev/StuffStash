baaa987b25143753106cbd3c4f96ea8ca5cbde96
mpochowi <90714974+mpochowi@users.noreply.github.com>
2022-12-05 13:41:49 -0500

    Style: added word stockroom to delete button * updated the delete button under stockroom view to include the word "stockroom" * button now says delete stockroom

--- Stuff-Stash/client/src/components/StockRoomViewDashboard.jsx
+++ Stuff-Stash/client/src/components/StockRoomViewDashboard.jsx
@@ -7,7 +7,7 @@ import Axios from "axios";
 
 const orgName = ReactSession.get("selectedOrg");
 
-const StockRoomViewDashboard = ({orgName}) => {
+const StockRoomViewDashboard = ({ orgName }) => {
   const [listOfStockRoom, setListOfStockRoom] = useState([]);
   const [org, setOrg] = useState("");
   const [listOfAssets, setListOfAssets] = useState([]);
@@ -24,14 +24,12 @@ const StockRoomViewDashboard = ({orgName}) => {
 
   useEffect(() => {
     console.log(orgName);
-    Axios.get(
-      `http://localhost:3000/api/v1/users/viewstock/${orgName}`
-    )
+    Axios.get(`http://localhost:3000/api/v1/users/viewstock/${orgName}`)
       .then((response) => {
         setListOfStockRoom(response.data);
         setError(null);
       })
-      .catch((err) => { 
+      .catch((err) => {
         setListOfStockRoom("");
         setError(err.response.data.msg);
       });
@@ -41,86 +39,53 @@ const StockRoomViewDashboard = ({orgName}) => {
     setOrg(orgName);
 
     //hide previously displayed create asset button
-    if(ReactSession.get("selectedStockroom") == null || document.getElementsByClassName("createButton") != null)
-    {
+    if (
+      ReactSession.get("selectedStockroom") == null ||
+      document.getElementsByClassName("createButton") != null
+    ) {
       var buttons = document.getElementsByClassName("createButton");
       var buttonDelete = document.getElementsByClassName("deleteButton");
-      for (var i = 0; i < buttons.length; i ++) {
+      for (var i = 0; i < buttons.length; i++) {
         buttons[i].hidden = true;
-        buttonDelete[i].hidden=true;
-    }
+        buttonDelete[i].hidden = true;
+      }
       document.getElementsByClassName("createButton").hidden = true;
       document.getElementsByClassName("deleteButton").hidden = true;
-      
     }
 
     //This will set the stockroom session variable to the stockroom that the user just clicked on
     ReactSession.set("selectedStockroom", event.currentTarget.id);
     stockroomName = ReactSession.get("selectedStockroom");
     console.log("Selected Stockroom is currently " + stockroomName);
-    
 
     //display the create asset button for the correct stockroom
     document.getElementById(stockroomName + "create").hidden = false;
-    document.getElementById("delete"+stockroomName).hidden = false;
+    document.getElementById("delete" + stockroomName).hidden = false;
 
     // This will get all the assets under whatever stockroom the user just clicked on
-    Axios.get(`http://localhost:3000/api/v1/users/viewAssets/${orgName}/${stockroomName}`)
+    Axios.get(
+      `http://localhost:3000/api/v1/users/viewAssets/${orgName}/${stockroomName}`
+    )
       .then((response) => {
         setListOfAssets(response.data);
         // This may look delayed by one click but don't worry it is receiving the correct assets
-        //console.log(listOfAssets);  
+        //console.log(listOfAssets);
       })
       .catch((err) => {
         setError(err);
       });
 
-      document.getElementById("AssetList").removeAttribute("hidden");
+    document.getElementById("AssetList").removeAttribute("hidden");
   };
   const deleteStockroom = async (event) => {
     try {
-      const response = await fetch("http://localhost:3000/api/v1/deleteStockroom", {
-        method: "POST",
-        body: JSON.stringify({
-          org,
-          stockroomName,
-        }),
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-        },
-      }
-    );
-
-    if (!response.ok) {
-      throw new Error(`Error! status: ${response.status}`);
-    }
-
-    const result = await response.json();
-
-    console.log("result is: ", JSON.stringify(result, null, 4));
-  } catch (err) {
-  } finally {
-    window.location.reload();
-  }
-};
-
-  const handleClick = async (event) => {
-    try {
       const response = await fetch(
-        "http://localhost:3000/api/v1/addAsset",
+        "http://localhost:3000/api/v1/deleteStockroom",
         {
           method: "POST",
           body: JSON.stringify({
-            stockroomName: stockroomName,
-            asset: {
-              identifier: "identifier",
-              category: "category",
-              isAvailable: "true",
-              condition: "mint",
-              serialCode: "Undefined",
-              warranty: "Undefined"
-            }
+            org,
+            stockroomName,
           }),
           headers: {
             "Content-Type": "application/json",
@@ -142,9 +107,43 @@ const StockRoomViewDashboard = ({orgName}) => {
     }
   };
 
+  const handleClick = async (event) => {
+    try {
+      const response = await fetch("http://localhost:3000/api/v1/addAsset", {
+        method: "POST",
+        body: JSON.stringify({
+          stockroomName: stockroomName,
+          asset: {
+            identifier: "identifier",
+            category: "category",
+            isAvailable: "true",
+            condition: "mint",
+            serialCode: "Undefined",
+            warranty: "Undefined",
+          },
+        }),
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error! status: ${response.status}`);
+      }
+
+      const result = await response.json();
+
+      console.log("result is: ", JSON.stringify(result, null, 4));
+    } catch (err) {
+    } finally {
+      window.location.reload();
+    }
+  };
+
   return (
     <React.Fragment>
-       {error && <div>{error}</div>}
+      {error && <div>{error}</div>}
       {Object.entries(listOfStockRoom).map(([key, value]) => {
         return (
           <li className="list-group-item bg-transparent" key={value.name}>
@@ -165,22 +164,23 @@ const StockRoomViewDashboard = ({orgName}) => {
                     </button>
                   </div>
                   <button
-                      class = "createButton"
-                      id={name[1] + "create"}
-                      onClick={handleClick}
-                      type="hidden"
-                      hidden={true}
-                      >
-                      Create Asset
-                    </button>
+                    class="createButton"
+                    id={name[1] + "create"}
+                    onClick={handleClick}
+                    type="hidden"
+                    hidden={true}
+                  >
+                    Create Asset
+                  </button>
 
-                    <button 
-                      id={"delete" + name[1]}  
-                      class = "deleteButton" 
-                      onClick={deleteStockroom} 
-                      type="hidden" 
-                      hidden={true}>
-                    Delete
+                  <button
+                    id={"delete" + name[1]}
+                    class="deleteButton"
+                    onClick={deleteStockroom}
+                    type="hidden"
+                    hidden={true}
+                  >
+                    Delete Stockroom
                   </button>
                 </div>
               );
@@ -190,18 +190,27 @@ const StockRoomViewDashboard = ({orgName}) => {
       })}
       <div id="AssetList" hidden="hidden">
         {Object.entries(listOfAssets).map(([key, value]) => {
-          return (     
+          return (
             <>
-              { listOfAssets[0].assets.length === 0 && <h4>{stockroomName} currently has no assets to be viewed</h4>}
+              {listOfAssets[0].assets.length === 0 && (
+                <h4>{stockroomName} currently has no assets to be viewed</h4>
+              )}
               {Object.entries(value.assets).map((name, key) => {
                 return (
                   <div>
                     {console.log(name[1])}
-                    <Assetcard name={name[1].identifier} avail={name[1].isAvailable} cond={name[1].condition} date={name[1].warranty} serialCode={name[1].serialCode} category={name[1].category}/>
+                    <Assetcard
+                      name={name[1].identifier}
+                      avail={name[1].isAvailable}
+                      cond={name[1].condition}
+                      date={name[1].warranty}
+                      serialCode={name[1].serialCode}
+                      category={name[1].category}
+                    />
                   </div>
-                )
+                );
               })}
-            </> 
+            </>
           );
         })}
       </div>
